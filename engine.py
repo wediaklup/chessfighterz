e7bfa671645ea1754f6d2dcd41b646b644fb3f79
wediaklup <101984195+wediaklup@users.noreply.github.com>
2022-10-18 16:21:13 +0200

    image drawing

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 RATE  = 60
 WIDTH, HEIGHT = 1600, 900
@@ -10,3 +11,10 @@ WHITE = (255, 255, 255)
 def win_init():
     WINDOW.fill(WHITE)
     pygame.display.update()
+
+class draw:
+    @staticmethod
+    def img(name: str, x: int, y: int):
+        """Draws an image with the given name from the assets directory at the specified coordinates (x, y). File extenson is required."""
+        img = pygame.image.load(os.path.join("Assets", name))
+        WINDOW.blit(img, (x, y))
